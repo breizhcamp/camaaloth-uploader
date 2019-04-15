2d6330c60e9edc2bff764ebd1e79b58aa062c4c4
Yoann Dubreuil <yoann.dubreuil@gmail.com>
2019-04-15 21:52:42 +0200

    Don't create ThumbGeneratorApplication bean

--- src/main/java/org/breizhcamp/video/uploader/CamaalothUploaderApplication.kt
+++ src/main/java/org/breizhcamp/video/uploader/CamaalothUploaderApplication.kt
@@ -1,11 +1,15 @@
 package org.breizhcamp.video.uploader
 
+import org.breizhcamp.video.uploader.thumb.ThumbGeneratorApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.FilterType
 
 @SpringBootApplication
 @EnableConfigurationProperties(CamaalothUploaderProps::class)
+@ComponentScan(excludeFilters = arrayOf(ComponentScan.Filter(value = [(ThumbGeneratorApplication::class)], type = FilterType.ASSIGNABLE_TYPE)))
 class CamaalothUploaderApplication
 
 fun main(args: Array<String>) {
