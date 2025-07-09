0f4a49525a2a67dc631a4e5fc1d7025b011acd86
Yoann Dubreuil <yoann.dubreuil@gmail.com>
2025-07-09 18:33:29 +0200

    suppression du post-compresseur
    On avait déjà un compresseur sur les tables de mix cette année

--- scripts/normalize.sh
+++ scripts/normalize.sh
@@ -11,7 +11,6 @@ for video in "${videos[@]}" ; do
       -pr \
       -c:a aac -ar 48000 -b:a 96K \
       -tp -3 -lrt 12 \
-      --post-filter acompressor=threshold=-18dB:ratio=2:attack=5:release=50:makeup=2,acompressor=threshold=-2dB:ratio=20:attack=0.01:release=5:detection=peak:knee=1 \
       -f -o "target/videos/${output_video}.mp4"
 
     ffmpeg -hide_banner -loglevel error -y -i "target/videos/${output_video}.mp4" "target/audiotracks/${output_video}.wav"
